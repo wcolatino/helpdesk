0a8db72a7ce1fe06ea2d050b948e22d6139aee1a
wcolatino <wcolatino@gmail.com>
2024-07-21 15:46:38 -0300

    Melhoria no exception Handler para argumentos inválidos nos end-points

--- src/main/java/com/colatino/helpdesk/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/colatino/helpdesk/resources/exceptions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 
@@ -56,6 +57,15 @@ public class ResourceExceptionHandler {
 
         // Manipula a mensagem de erro devolvendo o momento, o status, a mensagem e a URI do erro;
         StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "End-point não implementado", ex.getMessage(), request.getRequestURI());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<StandardError> resourceErrors(MethodArgumentTypeMismatchException ex, HttpServletRequest request){
+
+        // Manipula a mensagem de erro devolvendo o momento, o status, a mensagem e a URI do erro;
+        StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(), "End-point/argumento não implementado", ex.getMessage(), request.getRequestURI());
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
