d89a2f1776186e2e721d1083b7b6d68b2c6915e4
wcolatino <wcolatino@gmail.com>
2024-07-21 13:16:34 -0300

    Criado end-point de findALL para tecnicos

--- src/main/java/com/colatino/helpdesk/resources/TecnicoResource.java
+++ src/main/java/com/colatino/helpdesk/resources/TecnicoResource.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping("/tecnicos")
 public class TecnicoResource {
@@ -22,5 +25,12 @@ public class TecnicoResource {
     public ResponseEntity<TecnicoDTO> findaById(@PathVariable Integer id){ //Returno o DTO ao invés da entidade
         Tecnico obj = tecnicoService.findById(id);
         return ResponseEntity.ok(new TecnicoDTO(obj)); //Passo o DTO como objeto e o obj como parâmetro, já que tenho construtor obrigado.
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TecnicoDTO>> findAll(){
+        List<Tecnico> list = tecnicoService.findAll();
+        List<TecnicoDTO> listDTO = list.stream().map(obj -> new TecnicoDTO(obj)).collect(Collectors.toList()); //Converte a lista de Tecnicos em TecnicoDTO;
+        return ResponseEntity.ok().body(listDTO);
     }
 }
--- src/main/java/com/colatino/helpdesk/services/TecnicoService.java
+++ src/main/java/com/colatino/helpdesk/services/TecnicoService.java
@@ -6,6 +6,7 @@ import com.colatino.helpdesk.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,6 +18,10 @@ public class TecnicoService {
     public Tecnico findById(Integer id){
        Optional<Tecnico> tecnico = repository.findById(id);
        return tecnico.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado! Id: "+id));
+    }
+
+    public List<Tecnico> findAll(){
+        return repository.findAll();
     }
 
 }
